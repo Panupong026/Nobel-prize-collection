e1412fa57ee84bf22c0e29ea63373b21513f9b30
panupong <panuponglovecal@gmail.com>
2022-11-30 15:38:43 +0700

    "move to nav"

--- noble-app/src/App.js
+++ noble-app/src/App.js
@@ -2,35 +2,37 @@ import './App.css';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { Route, Routes, Link, Navigate } from 'react-router-dom';
+import Home from './Home'
 import Nav from './components/Nav/Nav';
 import Search from './components/Search';
 import About from './components/About';
 import Content from './components/Content';
 
+
 const App = () => {
 
-  const [nobel, setNobel] = useState([])
-  
+  // const [nobel, setNobel] = useState([])
+
   return (
-    <div>
+    <div className='container'>
       <nav>
-      <header>
-        <h1 className='header'>
-          <Link to="/">
-            Noble Prize Collection
-            </Link>
+        <header headerContainer>
+          <h1 className='header'>
+            <Link to="/Home">Noble Prize Collection</Link>
+            <span className='about'>
+            <Link to="/about">About</Link>
+            </span>
           </h1>
-      </header>
-        <Link to="/about">About</Link>
+        </header>
       </nav>
-      <main className='body'>
         <Nav />
         <Search />
-        <Content nobel={nobel} setNobel={setNobel}/>
-      </main>
-      <Routes>
-      <Route path='/about' element={<About />}/>
-      </Routes>
+        {/* <Content nobel={nobel} setNobel={setNobel} /> */}
+
+        <Routes>
+          <Route path='/Home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
     </div>
   );
 }
