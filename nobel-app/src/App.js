f7ae6d62f2d0a0d1f8cdda3c89ae51c6dbf2cfb3
panupong <panuponglovecal@gmail.com>
2022-12-02 15:59:49 +0700

    "start search"

--- nobel-app/src/App.js
+++ nobel-app/src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { Route, Routes, Link, Navigate } from 'react-router-dom';
 import Home from './Home'
 import Nav from './components/Nav/Nav';
-import Search from './components/Search';
-import About from './components/About';
-import All from './components/categories/All';
 
 
 const App = () => {
@@ -14,22 +10,17 @@ const App = () => {
   return (
     <div className='container'>
       <header>
-          <h1 className='header'>
-            <Link to="/Home">Noble Prize Collection</Link>
-          </h1>
+        <h1 className='header'>
+          <Link to="/Home">Noble Prize Collection</Link>
+        </h1>
       </header>
-      <nav>
-      </nav>
-      <div className='body  '>
-        <Nav />
-        <Link to="/about">About</Link>
-        <Search />
-
+      <div className='body'>
+        <nav className='navBar'>
+          <Nav />
+        </nav>
         <Routes>
           <Route path='/Home' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/Content/All' element={<About />} />
-          <Route path='/Content/Chemistry' element={<About />} />
+          <Route path='/Nav' element={<Nav />} />
         </Routes>
       </div>
     </div>
--- nobel-app/src/components/Search.js
+++ nobel-app/src/components/Search.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Dropdown from "react-dropdown";
 
-const Search = () => {
-    const options = ["name", "year", "country"]
-    const defaultOption = options[0]
+const Search = ({ insert, setInsert }) => {
+    // const options = ["name", "year", "country"]
+    // const defaultOption = options[0]
+
+    let handleChange = (e) => {
+        setInsert(e.target.value)
+    }
+
+    console.log(insert)
+
+    let handleClick = (e) => {
+        e.preventDefault()
+        console.log(insert)
+    }
+
     return (
         <div>
-            <Dropdown options={options} value={defaultOption} placeholder="Select an option"  />
-            <input type="text" placeholder="insert" />
-            <input type="submit" />
+            {/* <Dropdown options={options} value={defaultOption} placeholder="Select an option"/> */}
+            <form onSubmit={handleClick}>
+                <input
+                    type="text"
+                    placeholder="insert"
+                    onChange={(e) => handleChange(e)}
+                    value={insert}
+                />
+                <input type="submit" />
+            </form>
         </div>
     )
 }
